bd4393cd6aab8fe2b35fda6a73abef98ee147d39
Erik Swanson <theerikswanson@gmail.com>
2016-09-17 19:57:58 -0400

    ch 2: notes, exercises, source files

--- 02-HelloHaskell/learn.hs
+++ 02-HelloHaskell/learn.hs
@@ -1,5 +1,14 @@
 module Learn where
 
-x = 10 * 5 _ y
+x = 10 * 5 + y
 myResult = x * 5
 y = 10
+
+-- Indentation matters:
+--
+-- Miss-indenting the `z =` declaration or the `in` block would be an
+-- error.
+foo x =
+    let y = x * 2
+        z = x ^ 2
+    in 2 * y * x
