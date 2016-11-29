8720db64a5b02dd3aee09d0f67ee6bd70aa83b55
Erik Swanson <theerikswanson@gmail.com>
2016-11-28 21:22:37 -0500

    First working Hspec tests

---
+++ 14-Testing/TestingPackage/Addition.hs
@@ -0,0 +1,11 @@
+module Addition where
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
