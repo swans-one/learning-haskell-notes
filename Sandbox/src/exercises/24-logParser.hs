f0b2cb33ea4a3f04070299dc91bdf25a112115ee
Erik Swanson <theerikswanson@gmail.com>
2017-03-31 21:09:34 -0400

    Working log parser

--- Sandbox/src/exercises/24-logParser.hs
+++ Sandbox/src/exercises/24-logParser.hs
@@ -1,8 +1,10 @@
 import Control.Applicative
+import qualified Data.List as L
 import qualified Data.Text as T
 
 import Text.Trifecta
 import Test.Hspec
+
 
 data Log = Log [Comment] [Day] deriving (Eq, Show)
 data Day = Day Date Comment [Entry] deriving (Eq, Show)
@@ -11,11 +13,18 @@ data Entry = Entry Time Activity Comment deriving (Eq, Show)
 type Comment = Maybe String
 type Time = Int
 type Activity = String
-type Date = String
+type Date = (Int, Int, Int)
 
 parseLog :: Parser Log
 parseLog = do
-  return $ Log [] []
+  blankLines
+  comments <- parseComments
+  blankLines
+  days <- many (parseDay <* blankLines)
+  return $ Log comments days
+
+parseDay :: Parser Day
+parseDay = liftA3 Day parseDate parseComment (some parseEntry)
 
 parseEntry :: Parser Entry
 parseEntry = do
@@ -24,6 +33,17 @@ parseEntry = do
   comment <- parseComment
   many (char '\n')
   return $ Entry time activity comment
+
+parseDate :: Parser Date
+parseDate = do
+  string "# "
+  year <- count 4 digit
+  char '-'
+  month <- count 2 digit
+  char '-'
+  day <- count 2 digit
+  many $ (char '\n' <|> char ' ')
+  return (read year, read month, read day)
 
 parseTime :: Parser Time
 parseTime = do
@@ -35,6 +55,9 @@ parseTime = do
 parseActivity :: Parser Activity
 parseActivity = char ' ' >> strip <$> sentences
 
+parseComments :: Parser [Comment]
+parseComments = many (Just <$> parseCommentString)
+
 parseComment :: Parser Comment
 parseComment = try (fmap Just parseCommentString) <|> return Nothing
 
@@ -45,6 +68,9 @@ parseCommentString = do
   com <- sentences
   some (char '\n')
   return com
+
+blankLines :: Parser String
+blankLines = many $ oneOf ['\n', ' ']
 
 sentences :: Parser String
 sentences = some (letter <|> char ' ' <|> punctuation)
@@ -59,12 +85,58 @@ maybeSuccess :: Result a -> Maybe a
 maybeSuccess (Success a) = Just a
 maybeSuccess _ = Nothing
 
+exampleDay :: String
+exampleDay = L.intercalate
+    "\n"
+    ["# 2025-02-05 -- This is a comment"
+    , "08:00 Breakfast"
+    , "09:00 Sanitizing moisture collector"
+    , "11:20 Exercise   -- Worked hard"
+    , "12:15 Lunch and reading"
+    ]
+
+exampleLog :: String
+exampleLog = L.intercalate
+    "\n"
+    [ ""
+    , "-- My personal log"
+    , "-- By Erik Swanson"
+    , ""
+    ,  "# 2025-02-05 -- This is a comment"
+    , "08:00 Breakfast"
+    , "09:00 Sanitizing moisture collector"
+    , "11:20 Exercise   -- Worked hard"
+    , "12:15 Lunch"
+    , "13:00 Reading"
+    , "22:00 Sleep -- good night"
+    , ""
+    , "# 2025-02-06"
+    , ""
+    , "08:00 Breakfast -- cereal"
+    , "09:45 Work"
+    , "17:00 Breath of the wild"
+    , "22:00 Sleep"
+    ]
+
+
 htest :: IO ()
 htest = hspec $ do
   describe "parseLog" $ do
     it "parses nothing" $ do
       let expected = Log [] []
           result = parseString parseLog mempty ""
+      maybeSuccess result `shouldBe` Just expected
+
+  describe "parseDay" $ do
+    it "parses and example day" $ do
+      let expected = Day (2025, 2, 5)
+                         (Just "This is a comment")
+                         [ Entry 480 "Breakfast" Nothing
+                         , Entry 540 "Sanitizing moisture collector" Nothing
+                         , Entry 680 "Exercise" (Just "Worked hard")
+                         , Entry 735 "Lunch and reading" Nothing
+                         ]
+          result = parseString parseDay mempty exampleDay
       maybeSuccess result `shouldBe` Just expected
 
   describe "parseEntry" $ do
@@ -76,6 +148,17 @@ htest = hspec $ do
     it "parses with a comment" $ do
       let expected = Entry 20 "Eat food" (Just "always do this")
           result = parseString parseEntry mempty "00:20 Eat food -- always do this\n"
+      maybeSuccess result `shouldBe` Just expected
+
+  describe "parseDate" $ do
+    it "parses a valid date with comment" $ do
+      let expected = (2050, 12, 8)
+          result = parseString parseDate mempty "# 2050-12-08 -- comment"
+      maybeSuccess result `shouldBe` Just expected
+
+    it "parses a valid date with newline" $ do
+      let expected = (2050, 12, 8)
+          result = parseString parseDate mempty "# 2050-12-08 \n"
       maybeSuccess result `shouldBe` Just expected
 
   describe "parseTime" $ do
